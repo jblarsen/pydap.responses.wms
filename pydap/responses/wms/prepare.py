505d02a947760ab413f00efdc00ed89cea18fd64
Jesper Baasch-Larsen <jla@fcoo.dk>
2014-03-19 22:32:47 +0100

    adding bounds to 1d dimension variables

--- pydap/responses/wms/prepare.py
+++ pydap/responses/wms/prepare.py
@@ -1,6 +1,6 @@
 import sys
 
-import numpy 
+import numpy as np
 
 from pydap.handlers.netcdf import nc, var_attrs
 from pydap.responses.wms import fix_data
@@ -17,7 +17,30 @@ def prepare_netcdf():
     # set actual range
     for name, var in f.variables.items():
         if name in f.dimensions or hasattr(var, 'actual_range'): continue
-        data = fix_data(numpy.asarray(var[:]), var_attrs(var)) 
-        var.actual_range = numpy.amin(data), numpy.amax(data)
+        data = fix_data(np.asarray(var[:]), var_attrs(var)) 
+        var.actual_range = np.amin(data), np.amax(data)
+
+    # create bounds for 1d dimension variables
+    for name, dim in f.dimensions.items():
+        var = f.variables[name]
+        if hasattr(var, 'bounds'): continue
+        ndims = len(var.shape)
+        # We currently only do this for 1d variables
+        if ndims > 1: continue
+        vert_dim = u'nv'
+        if vert_dim not in f.dimensions.keys():
+            nv = f.createDimension(vert_dim, 2*ndims)
+        bnds_name = name + '_bnds'
+        var.bounds = bnds_name
+        bdims = var.dimensions + (vert_dim,)
+        bounds = f.createVariable(bnds_name, var.dtype, bdims)
+        dv = 0.5*abs(var[-1]-var[0])/(len(var)-1)
+        lbounds = var[:] - dv
+        ubounds = var[:] + dv
+        bounds[:,0] = lbounds
+        bounds[:,1] = ubounds
 
     f.close()
+
+if __name__ == '__main__':
+    sys.exit(prepare_netcdf())
