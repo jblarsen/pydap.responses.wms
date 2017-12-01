29fe1e13e6a8224f4592d297ce605f0905ac58c8
Jesper Baasch-Larsen <jla@fcoo.dk>
2017-12-01 08:46:57 +0100

    removed python2 support + cleanup

--- src/pydap/responses/wms/cache_middleware.py
+++ src/pydap/responses/wms/cache_middleware.py
@@ -1,7 +1,10 @@
-from __future__ import division
+"""
+Factory function for producing beaker cache WSGI middleware.
+"""
 
 from beaker.middleware import CacheMiddleware
 def make_cache(app, global_conf, **local_conf):
+    """Return beaker cache WSGI app."""
     conf = global_conf.copy()
     conf.update(local_conf)
     return CacheMiddleware(app, conf)
